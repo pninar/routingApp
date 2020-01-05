f10681465d7763dc5a54a7365d11fe2d5a7ebdaf
Pnina Rosenstark <Pnina@merozsw.co.il>
2020-01-05 15:30:59 +0200

    changes

--- src/interfaces/patient.interface.ts
+++ src/interfaces/patient.interface.ts
@@ -1,11 +1,11 @@
 import { IPhoneNumber } from './phoneNumber.interface';
+import { IId } from './id.interface';
 
-export interface IPatient {
-    id: number;
+export interface IPatient extends IId {
     firstName: string;
     lastName: string;
-    contactPreference: string;
+    // contactPreference: string;
     email: string;
     phone?: number;
-    phoneNumbers: IPhoneNumber[];
+    // phoneNumbers: IPhoneNumber[];
 }
