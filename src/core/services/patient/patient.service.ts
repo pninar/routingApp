216ff0ff609b96967a41eef5bf8e068094aefc6d
Pnina Rosenstark <Pnina@merozsw.co.il>
2020-01-05 15:31:16 +0200

    added behaviorSubject

--- src/core/services/patient/patient.service.ts
+++ src/core/services/patient/patient.service.ts
@@ -1,38 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 import { RestService } from '../rest/rest.service';
 import { IPatient } from 'src/interfaces/patient.interface';
+import { filter } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class PatientService extends RestService {
   private relativeUrl: string = '/patients/';
 
+  private patient: BehaviorSubject<IPatient> = new BehaviorSubject(null);
+  currentPatient = this.patient.asObservable();
+
   constructor(protected httpClient: HttpClient) { super(httpClient); }
 
-  getPatients(): Observable<IPatient[]> {
-    // return this.httpClient.get<IPatient[]>(this.baseUrl);
+  changePatient(patient: IPatient) {
+    this.patient.next(patient);
+  }
+
+  getPatients(firstName: string = '', lastName: string = ''): Observable<IPatient[]> {
     return this.getList(this.relativeUrl);
   }
 
   getPatient(id: number): Observable<IPatient> {
     return this.get(`${this.relativeUrl}${id.toString()}`);
-    // return this.httpClient.get<IPatient>(`${this.baseUrl}/${id}`);
   }
 
   addPatient(patient: IPatient): Observable<IPatient> {
     return this.post(this.relativeUrl, patient);
-    // return this.httpClient.post<IPatient>(this.baseUrl, patient, httpOptions);
   }
 
   updatePatient(patient: IPatient): Observable<void> {
     return this.put(`${this.relativeUrl}/${patient.id}`, patient);
-    // return this.httpClient.put<void>(`${this.baseUrl}/${patient.id}`, patient, httpOptions);
   }
 
   deletePatient(id: number): Observable<void> {
     return this.delete(this.relativeUrl, id);
-    // return this.httpClient.delete<void>(`${this.baseUrl}/${id}`);
   }
 }
